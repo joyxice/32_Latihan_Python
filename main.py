9007d8a07416759fc4d927387b9406defb3627ad
superjoyxice <thatme749@gmail.com>
2026-08-24 09:05:28 +0700

    commit kedua

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = int(input('Masukkan angka : '))
-
-if x % 2 == 0:
-    print('genap')
-else:
-    print('ganjil')
+jalan = 'y'
+while True:
+    if jalan == 'y' or jalan == 'Y':
+        x = int(input('Masukkan angka: '))
+        print(f'bilangan {x} adalah genap') if x % 2 == 0 else print(f'bilangan {x} adalah ganjil')
+    else:
+        break
+    jalan = str(input('Lanjut? (y/n): '))
